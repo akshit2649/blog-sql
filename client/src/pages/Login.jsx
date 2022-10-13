14ac365a2e9937ca538d188a5642931c3a7cff47
Akshit Thakur <akshit2649@gmail.com>
2022-10-14 01:15:42 +0530

    login & register design

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className="auth">
       <h1>Login</h1>
       <form>
-        <input type="text" placeholder="usename" />
-        <input type="password" placeholder="password" />
+        <input required type="text" placeholder="usename" />
+        <input required type="password" placeholder="password" />
         <button>Login</button>
+        <p>This is an error!</p>
+        <span>
+          Don't you have an account?<Link to="/register"> Register</Link>
+        </span>
       </form>
     </div>
   );
