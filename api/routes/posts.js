6f8742c833ed682851112e426557961c30c1e5a1
Akshit Thakur <akshit2649@gmail.com>
2022-10-16 14:19:02 +0530

    add to sql

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,8 +1,5 @@
 import express from "express";
-import { addPost } from "../controllers/post.js";
 
 const router = express.Router();
-
-router.get("/", addPost);
 
 export default router;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,54 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/register", inputs);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="auth ww">
       <h1>Login</h1>
       <form>
-        <input required type="text" placeholder="usename" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button>Register</button>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Register</button>
         <p>This is an error!</p>
         <span>
           Don't you have an account?<Link to="/login"> Login</Link>
